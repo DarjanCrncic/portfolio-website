328af5b99211f95fbf761c44c7da5c3d5644f1e0
DarjanCrncic <darjancr@gmail.com>
2021-11-14 10:47:34 +0100

    Finished course page layout.

--- src/components/Courses/CourseBlock.js
+++ src/components/Courses/CourseBlock.js
@@ -3,10 +3,10 @@ import classes from "./CourseBlock.module.css";
 const CourseBlock = (props) => {
   return (
     <div className={classes.course}>
-      <img src="logo192.png" alt="course"></img>
+      <img src={props.course_image} alt="course"></img>
       <div className={classes.textContainer}>
         <h3>{props.children}</h3>
-        <a href="https://www.udemy.com/course/spring-boot-security-and-oauth2/">See course</a>
+        <a href={props.link}>See course</a>
       </div>
     </div>
   );
--- src/components/Courses/CoursesContainer.js
+++ src/components/Courses/CoursesContainer.js
@@ -3,32 +3,25 @@ import CourseBlock from "./CourseBlock";
 import classes from "./CoursesContainer.module.css";
 
 const CoursesContainer = () => {
+  let items = [];
+  const generateCourse = () => {
+    return (
+      <CourseBlock
+        course_image="logo192.png"
+        link="https://www.w3schools.com/css/css_overflow.asp"
+      >
+        Spring Boot Security and oAuth2 in depth from scratch
+      </CourseBlock>
+    );
+  };
+
+  for (let i = 0; i < 20; i++) {
+    items.push(generateCourse());
+  }
+
   return (
     <div className={classes.container + " " + classes["custom-scrollbar"]}>
-      <CourseBlock>
-        Spring Boot Security and oAuth2 in depth from scratch
-      </CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>
-        Spring Boot Security and oAuth2 in depth from scratch
-      </CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>
-        Spring Boot Security and oAuth2 in depth from scratch
-      </CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>
-        Spring Boot Security and oAuth2 in depth from scratch
-      </CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
-      <CourseBlock>Titple</CourseBlock>
+      {items}
     </div>
   );
 };
