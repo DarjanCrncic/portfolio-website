d676d13434d81702c89fd934028e69663b4e03f8
DarjanCrncic <darjancr@gmail.com>
2021-11-27 10:19:07 +0100

    Updated the list of courses and rearranged images in public folder.

--- src/components/Projects/project-list.js
+++ src/components/Projects/project-list.js
@@ -1,31 +1,31 @@
 export const projects = [
   {
     name: "Image Gallery",
-    image: "gallery.png",
+    image: "projects/gallery.png",
     link: "https://image-paintings-gallery.herokuapp.com/home",
     description: "Portfolio website for an artist, painter or photographer. Created using react.js."
   },
   {
     name: "Vinyl Collections",
-    image: "v-collections.png",
+    image: "projects/v-collections.png",
     link: "https://vinyl-collections.herokuapp.com",
     description: "Vinyl record collection site. Users can enter and store information about their record collections. Created using node.js and EJS."
   },
   {
     name: "MiniFaceApp",
-    image: "minifaceapp.png",
+    image: "projects/minifaceapp.png",
     link: "https://mini-face-app.herokuapp.com/",
     description: "Facebook clone created using Spring Boot and Jquery. After registration and login users can add friends, create groups, enter groups, chat, create posts and more. Unfortunately the page is not very responsive."
   },
   {
-    name: "Quiz App",
-    image: "quiz.png",
+    name: "Quizzes",
+    image: "projects/quiz.png",
     link: "https://quiz-app-react-front.herokuapp.com/home",
     description: "Quiz application where users can select different types of quizzes and play them. Only option to login/sign up is using Facebook. Users can also compare their statistics to their facebook friends. App was created using Spring Boot and React.js." 
   },
   {
     name: "OPTICS visualizer",
-    image: "optics.png",
+    image: "projects/optics.png",
     link: "https://darjancrncic.github.io/OPTICS-demonstration-web-app/",
     description: "App created using javascript and jQuery. It was designed to demonstrate OPTICS clustering algorithm. Users can modify clustering data using inbuilt data generation and optimize clustering results using various parameters."
   },
