cc56ddc418090f1b12ea0d23eb4fe718ce6eb623
DarjanCrncic <darjancr@gmail.com>
2021-12-15 20:21:54 +0100

    Improved PageDescription, modified Contact section,

--- src/components/Common/PageDescription.js
+++ src/components/Common/PageDescription.js
@@ -1,12 +1,14 @@
 import React from "react";
-import TextBlock from "./TextBlock";
 import classes from "./PageDescription.module.css";
 
 const PageDescription = (props) => {
   return (
-    <div className={classes.container}>
-      <TextBlock>{props.children}</TextBlock>
-    </div>
+    <React.Fragment>
+      <div className={classes.container}>
+        <div className={classes.background}>{props.children}</div>
+      </div>
+      <hr className={classes.break} />
+    </React.Fragment>
   );
 };
 
--- src/components/Contact/ContactPage.js
+++ src/components/Contact/ContactPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Page from '../Common/Page';
+import PageDescription from '../Common/PageDescription';
 import Title from '../Common/Title';
 import ContactData from './ContactData';
 
@@ -7,6 +8,9 @@ const ContactPage = () => {
   return (
     <Page scroll="contact">
       <Title>Contact</Title>
+      <PageDescription>
+        If you have any questions for me or are interested in working with me in the future, please feel free to contact me.
+      </PageDescription>
       <ContactData />
     </Page>
   )
