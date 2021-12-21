38aeeaa0aca0b9051fff911453463ee4198bc506
DarjanCrncic <darjancr@gmail.com>
2021-12-21 22:17:47 +0100

    .

--- src/components/Projects/project-list.js
+++ src/components/Projects/project-list.js
@@ -1,14 +1,14 @@
 export const projects = [
   {
     name: "Image Gallery",
-    image: "projects/gallery.png",
+    image: "/projects/gallery.png",
     link: "https://image-paintings-gallery.herokuapp.com/home",
     description:
       "Portfolio website for an artist, painter or photographer. Created using Node.js and React. The backend is quite small and is used only for serving static content and handling the contact form (sending emails). The frontend is built using Material-UI.",
   },
   {
     name: "Vinyl Collections",
-    image: "projects/v-collections.png",
+    image: "/projects/v-collections.png",
     link: "https://vinyl-collections.herokuapp.com",
     description:
       'Vinyl record collection site. Users can enter and store information about their collections. Created using Node.js with EJS as the template engine. This is my first "major" web-application.',
@@ -17,7 +17,7 @@ export const projects = [
   },
   {
     name: "MiniFaceApp",
-    image: "projects/minifaceapp.png",
+    image: "/projects/minifaceapp.png",
     link: "https://mini-face-app.herokuapp.com/",
     description:
       "Facebook clone created using Spring Boot and Jquery. After registering, users can add friends, create groups, chat, create posts and more. This is the first application I created using Spring Boot. Unfortunately the page is not very responsive.",
@@ -26,14 +26,14 @@ export const projects = [
   },
   {
     name: "Quizzes",
-    image: "projects/quiz.png",
+    image: "/projects/quiz.png",
     link: "https://quiz-app-react-front.herokuapp.com/home",
     description:
       "Quiz application where users can select different types of quizzes and play them. Users can also compare their statistics to their facebook friends. The only supported option is to login/sign-up using Facebook. App was created using Spring Boot and React.js.",
   },
   {
     name: "OPTICS visualizer",
-    image: "projects/optics.png",
+    image: "/projects/optics.png",
     link: "https://darjancrncic.github.io/OPTICS-demonstration-web-app/",
     description:
       "This app is a visual representation/demonstration of OPTICS clustering algorithm which was a very important part of my thesis, Recognizing Time-Frequency Clustering in Ultrasound Emission Spectra. Users can modify clustering data using inbuilt data generation and optimize clustering results using various parameters. The app was created using javascript and jQuery.",
