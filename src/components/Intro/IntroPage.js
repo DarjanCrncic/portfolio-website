01c60e5bfbdf9ef33a7b8f25f95c71bb40987285
Darjan Crnčić <darjancr@gmail.com>
2021-12-22 13:47:38 +0100

    Correcting a grammer mistake.

--- src/components/Intro/IntroPage.js
+++ src/components/Intro/IntroPage.js
@@ -10,7 +10,7 @@ const IntroPage = () => {
       <div className={classes.textHolder}>
         <ImageElement />
         <TextBlock style={{width: "60%", margin: "auto", boxShadow: "none", textAlign: "center"}}>
-          Hi there! My name is Darjan and welcome to my site!. I'm a young developer focusing in backend development with Java and Spring Boot. 
+          Hi there! My name is Darjan and welcome to my site! I'm a young developer focusing in backend development with Java and Spring Boot. 
           The idea behind this website is to showcase me, my skills and my work through something other than a traditional CV.
         </TextBlock>
       </div>
